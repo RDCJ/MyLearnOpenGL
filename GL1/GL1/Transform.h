ceb1f753fe8692a90d6102778d9525fc383d5ca6
RDCJ <1175024349@qq.com>
2024-04-12 13:21:48 +0800

    Transform支持绕轴旋转

--- GL1/GL1/Transform.h
+++ GL1/GL1/Transform.h
@@ -9,11 +9,16 @@ class Transform
 		glm::vec3 position;
 		glm::vec3 scale;
 
+		glm::vec3 rotate_axis;
+		float rotate_angle;
+
 		glm::mat4 GetModel() const
 		{
 			glm::mat4 model = glm::mat4(1);
 			model = glm::translate(model, position);
 			model = glm::scale(model, scale);
+			if (glm::length(rotate_axis) > 0)
+				model = glm::rotate(model, glm::radians(rotate_angle), rotate_axis);
 			return model;
 		}
 
@@ -21,10 +26,16 @@ class Transform
 		{
 			position = glm::vec3(0);
 			scale = glm::vec3(1);
+			rotate_axis = glm::vec3(0);
+			rotate_angle = 0;
 		}
 
 		Transform(const glm::vec3& position, const glm::vec3& scale)
-			: position(position), scale(scale)
+			: position(position), scale(scale), rotate_axis(glm::vec3(0)), rotate_angle(0)
+		{}
+
+		Transform(const glm::vec3& position, const glm::vec3& scale, const glm::vec3& rotate_axis, float rotate_angle)
+			: position(position), scale(scale), rotate_axis(rotate_axis), rotate_angle(rotate_angle)
 		{}
 };
 #endif // !TRANSFORM
